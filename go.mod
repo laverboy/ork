d80ccf3cf76d5169c5fc0e8c2ef6ff0172c3d3fa
laverboy <matthew.laver@bbc.co.uk>
2021-01-27 14:28:38 +0000

    OFFTICKET fix zipping!!

--- go.mod
+++ go.mod
@@ -1,16 +1,3 @@
 module ork
 
 go 1.14
-
-require (
-	github.com/Microsoft/go-winio v0.4.14 // indirect
-	github.com/davecgh/go-spew v1.1.1
-	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v1.13.1
-	github.com/docker/go-connections v0.4.0
-	github.com/docker/go-units v0.4.0 // indirect
-	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
-	github.com/stretchr/testify v1.6.1 // indirect
-	golang.org/x/net v0.0.0-20200707034311-ab3426394381 // indirect
-)
--- utils/zip.go
+++ utils/zip.go
@@ -2,8 +2,11 @@ package utils
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 func ZipFiles(filename string, files []string) error {
@@ -25,37 +28,81 @@ func ZipFiles(filename string, files []string) error {
 	return nil
 }
 
-func AddFileToZip(zipWriter *zip.Writer, filename string) error {
-	fileToZip, err := os.Open(filename)
-	if err != nil {
-		return err
+func AddFileToZip(zw *zip.Writer, src string) error {
+	originalParent := ""
+	if strings.Contains(src, "resources") {
+		originalParent = "resources/"
 	}
-	defer fileToZip.Close()
-
-	// Get the file information
-	info, err := fileToZip.Stat()
-	if err != nil {
-		return err
+	if strings.Contains(src, "cosmos") {
+		originalParent = "cosmos/"
 	}
 
-	header, err := zip.FileInfoHeader(info)
-	if err != nil {
-		return err
-	}
+	return filepath.Walk(src, func(file string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-	// Using FileInfoHeader() above only uses the basename of the file. If we want
-	// to preserve the folder structure we can overwrite this with the full path.
-	// un-comment to use
-	// header.Name = filename
+		if fi.IsDir() {
+			return nil
+		}
 
-	// Change to deflate to gain better compression
-	// see http://golang.org/pkg/archive/zip/#pkg-constants
-	header.Method = zip.Deflate
+		header, err := zip.FileInfoHeader(fi)
+		if err != nil {
+			return err
+		}
 
-	writer, err := zipWriter.CreateHeader(header)
-	if err != nil {
-		return err
-	}
-	_, err = io.Copy(writer, fileToZip)
-	return err
+		if isSymlink(fi) {
+			// file body for symlinks is the symlink target
+			linkTarget, err := os.Readlink(file)
+			if err != nil {
+				return fmt.Errorf("%s: readlink: %v", fi.Name(), err)
+			}
+
+			linkPath := filepath.Join(filepath.Dir(file), linkTarget)
+
+			fileInfo, err := os.Stat(linkPath)
+			if err != nil {
+				return err
+			}
+
+			h, err := zip.FileInfoHeader(fileInfo)
+			if err != nil {
+				return err
+			}
+
+			header = h
+			file = linkPath
+		}
+
+		// need to add parent dir for required dirs back in
+		header.Name = originalParent + header.Name
+
+		header.Method = zip.Store
+		// write the header
+		writer, err := zw.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		// open files for zipping
+		f, err := os.Open(file)
+		if err != nil {
+			return err
+		}
+
+		// copy file data into zip writer
+		if _, err := io.Copy(writer, f); err != nil {
+			return err
+		}
+
+		// manually close here after each file operation; deferring would cause each file close
+		// to wait until all operations have completed.
+		f.Close()
+
+		return nil
+	})
+}
+
+func isSymlink(fi os.FileInfo) bool {
+	return fi.Mode()&os.ModeSymlink != 0
 }
